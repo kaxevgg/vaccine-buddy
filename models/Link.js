6888d6706f4b1c5ef1708ccd2c13aa6ae9c1ca58
Arpit Bansal <hi@arpitbansal.com>
2021-05-19 02:49:07 +0800

    Initial commit

---
+++ models/Link.js
@@ -0,0 +1,12 @@
+// const bcrypt = require('bcrypt');
+// const crypto = require('crypto');
+// const mongoose = require('mongoose');
+
+// const linkSchema = new mongoose.Schema({
+//   linkUrl: String,
+//   domain: String
+// }, { timestamps: true });
+
+// const Link = mongoose.model('Link', linkSchema);
+
+// module.exports = Link;
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+"use strict"
+
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
+
+var txnId;
+
+/* GET home page. */
+router.get('/generateOTP', function(req, res, next) {
+  var data = JSON.stringify({"secret":"U2FsdGVkX1+VTlP0So1QxL6tbZmIty6lx41dE0iN09YJTzJwqYwC06FCG90Sw8h7qgJhN+jW+TGYvKPDXOEv0Q==","mobile":"9619127647"});
+
+  var config = {
+    method: 'post',
+    url: 'https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP',
+    headers: { 
+      'Content-Type': 'application/json',
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+});
+
+module.exports = router;
